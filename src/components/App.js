8f1183fe1aa954816a1b73b934908521d9e9c4d5
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-25 14:02:38 -0600

    complete lab of cute pups

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+
+import React, { useState, useEffect } from 'react'
+
+export default function App() {
+    const [dogPicture, setDogPicture] = useState("")
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(r => r.json())
+            .then((data) => {
+                setDogPicture(data.message)
+            })
+    }, [])
+
+    if (!dogPicture) return <p>Loading next cute dog...</p>
+
+
+    return <img src={dogPicture} alt="A very cute, but random dog" />
+
+
+}
